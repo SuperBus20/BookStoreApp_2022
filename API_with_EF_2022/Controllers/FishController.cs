71a3b38be68d501bdd822d0e8d4bb0d156234971
June Dougherty <zachharlandougherty@gmail.com>
2023-02-20 20:58:28 -0500

    added vs proj

---
+++ API_with_EF_2022/Controllers/FishController.cs
@@ -0,0 +1,28 @@
+﻿using API_with_EF_2022.DAL;
+using API_with_EF_2022.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_with_EF_2022.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class FishController : ControllerBase
+    {
+    FishRepository repo = new FishRepository();
+        [HttpPost("spawn")]
+        public Fish AddFish(string name,string classification, bool fresh,bool salt)
+        {
+            Fish newFish = new Fish
+            {
+                Name = name,
+                Classification = classification,
+                SaltWater = salt,
+                FreshWater = fresh
+            };
+
+            return repo.AddFish(newFish);
+        }
+    }
+}
---
+++ API_with_EF_2022/DAL/FishRepository.cs
@@ -0,0 +1,21 @@
+﻿using API_with_EF_2022.Models;
+
+namespace API_with_EF_2022.DAL
+{
+    public class FishRepository
+    {
+        private GameContext _dbContext = new GameContext();
+        public Fish AddFish(Fish fishToAdd)
+        {
+            _dbContext.Add(fishToAdd);
+            _dbContext.SaveChanges();
+
+            return GoFishingForLatest();
+        }
+
+        public Fish GoFishingForLatest()
+        {
+            return _dbContext.Fishes.OrderByDescending(fish => fish.Id).FirstOrDefault();
+        }
+    }
+}
